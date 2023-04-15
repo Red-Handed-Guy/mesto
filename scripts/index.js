ad8d6d2cd2d73a282debfc6d74a884841a6e44cf
Krasnorutskiy Kirill <dullsharpz@gmail.com>
2023-04-15 20:39:01 +0300

    fix: исправить ошибки первого ревью

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+// постоянные popup
+const popup = document.querySelector('.popup')
+const popupForm = popup.querySelector('.popup__container')
+const popupCloseButton = popup.querySelector('.popup__close-button')
+const popupName = popup.querySelector('.popup__textarea_type_name')
+const popupSubtitle = popup.querySelector('.popup__textarea_type_subtitle')
+
+// постоянные profile
+const profile = document.querySelector('.profile')
+const profileEditButton = profile.querySelector('.profile__edit-button')
+const profileTitle = profile.querySelector('.profile__title')
+const profileSubtitle = profile.querySelector('.profile__subtitle')
+
+//открытие popup
+function openPopup() {
+  popupName.value = profileTitle.textContent
+  popupSubtitle.value = profileSubtitle.textContent
+  popup.classList.add('popup_opened')
+}
+
+//закрытие popup
+function closePopup(event) {
+  event.preventDefault()
+  popup.classList.remove('popup_opened')
+}
+
+//сохранение popup
+function savePopup(event) {
+  closePopup(event)
+  profileTitle.textContent = popupName.value
+  profileSubtitle.textContent = popupSubtitle.value
+}
+
+profileEditButton.addEventListener('click', openPopup)
+popupCloseButton.addEventListener('click', closePopup)
+popupForm.addEventListener('submit', savePopup)
