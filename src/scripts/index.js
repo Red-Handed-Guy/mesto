50cbf69cf93234db3c1b6edbe8b861696ae1e92d
Krasnorutskiy Kirill <dullsharpz@gmail.com>
2023-06-22 01:43:56 +0300

    fix: изменить расположение js файлов с классами и переписать пути

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,7 +1,4 @@
 import '../pages/index.css'
-import { Card } from './Card.js'
-import { formValidators } from './FormValidator.js'
-import Section from './Section.js'
 import {
   initialCards,
   cardsContainer,
@@ -10,9 +7,13 @@ import {
   popupImgSelector,
   profileSelectors,
 } from '../utils/components.js'
-import PopupWithImage from './PopupWithImage.js'
-import PopupWithForm from './PopupWithForm.js'
-import UserInfo from './UserInfo.js'
+
+import { Card } from '../components/Card.js'
+import { formValidators } from '../components/FormValidator.js'
+import Section from '../components/Section.js'
+import PopupWithImage from '../components/PopupWithImage.js'
+import PopupWithForm from '../components/PopupWithForm.js'
+import UserInfo from '../components/UserInfo.js'
 
 //постоянные popupEdit
 const popupEdit = document.querySelector('.popup_type_edit')
